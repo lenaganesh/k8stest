0c9cdfbf6ab92167f25543c0925cb36d35d6770f
lenaganesh@yahoo.com <lenaganesh@yahoo.com>
2024-12-20 08:51:59 +0530

    Cucumber added

---
+++ src-1/test/java/com/isradlabs/k8stest/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isradlabs.k8stest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
