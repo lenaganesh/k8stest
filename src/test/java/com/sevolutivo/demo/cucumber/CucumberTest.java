342e8d80988dc117c195ecc41814d668db42679e
lenaganesh@yahoo.com <lenaganesh@yahoo.com>
2024-12-20 09:23:32 +0530

    Cucumber added

--- src/test/java/com/sevolutivo/demo/cucumber/CucumberTest.java
+++ src/test/java/com/sevolutivo/demo/cucumber/CucumberTest.java
@@ -1,14 +1,18 @@
 package com.sevolutivo.demo.cucumber;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import org.springframework.boot.test.context.SpringBootTest;
+import io.cucumber.spring.CucumberContextConfiguration;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("com/sevolutivo/demo/cucumber/features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.sevolutivo.demo.cucumber.stepDefs")
+@CucumberContextConfiguration
+@SpringBootTest
 public class CucumberTest {
 }
